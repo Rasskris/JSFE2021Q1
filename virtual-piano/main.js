2d082c9435fc9e53af7f86321880f966fa20b276
Rasskris <rassoshenko.kristina@yandex.ru>
2021-03-19 13:36:58 +0300

    feat: add switch to Notes/Letter
    When you click on the Notes / Letters switch, the display next to the piano keys changes the names of the notes or letters corresponding to these keys

--- virtual-piano/main.js
+++ virtual-piano/main.js
@@ -3,3 +3,29 @@ const playAudio = (src) => {
   audio.currentTime = 0;
   audio.play();
 };
+
+
+const btnContainer = document.querySelector('.btn-container');
+const buttons = document.querySelectorAll('.btn');
+const piano = document.querySelector('.piano');
+const pianoKeys = document.querySelectorAll('.piano-key');
+
+btnContainer.addEventListener('click', ({ target }) => {
+  buttons.forEach((button) => {
+    if (button.classList.contains('btn-active')) {
+      button.classList.remove('btn-active');
+    }
+  });
+
+  target.classList.add('btn-active');
+
+  if (target.classList.contains('btn-letters')) {
+    pianoKeys.forEach((pianoKey) => {
+      pianoKey.classList.add('piano-key-letter');
+    })
+  } else {
+    pianoKeys.forEach((pianoKey) => {
+      pianoKey.classList.remove('piano-key-letter');
+    })
+  }
+});
