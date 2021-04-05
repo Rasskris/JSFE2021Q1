ae6404b156e6ff3aadaf8877fbdaae239ef59a3b
Rasskris <rassoshenko.kristina@yandex.ru>
2021-04-05 23:27:19 +0300

    build: add webpack.config

---
+++ online-zoo/webpack.config.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const generateHtmlPlugin = (title) => {
+  return new HtmlWebpackPlugin({
+    title,
+    filename: `${title}.html`,
+    template: `./src/pages/${title}.html`,
+  });
+}
+
+const populateHtmlPlugins = (pagesArray) => {
+  res = [];
+  pagesArray.forEach(page => {
+    res.push(generateHtmlPlugin(page));
+  });
+  return res;
+};
+
+const pages = populateHtmlPlugins(["main", "map", "alligator", "gorilla", "eagle", "panda"]);
+
+let mode = "development";
+let target = "web";
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  ...pages,
+];
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  // Temporary workaround for 'browserslist' bug that is being patched in the near future
+  target = "browserslist";
+}
+
+
+module.exports = {
+  // mode defaults to 'production' if not set
+  mode: mode,
+
+  // This is unnecessary in Webpack 5, because it's the default.
+  entry: "./src/main.js",
+
+  output: {
+    // output path is required for `clean-webpack-plugin`
+    path: path.resolve(__dirname, "dist"),
+    // this places all images processed in an image folder
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          // without additional settings, this will reference .babelrc
+          loader: "babel-loader",
+          options: {
+            /**
+             * From the docs: When set, the given directory will be used
+             * to cache the results of the loader. Future webpack builds
+             * will attempt to read from the cache to avoid needing to run
+             * the potentially expensive Babel recompilation process on each run.
+             */
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: plugins,
+
+  target: target,
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".js"],
+  },
+
+  // required if using webpack-dev-server
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    writeToDisk: true,
+    port: 5500,
+  },
+};
