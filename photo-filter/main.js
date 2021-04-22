e9ef1084fe2ba7ca7c99906b4539b8bf92d8fcc8
Rasskris <rassoshenko.kristina@yandex.ru>
2021-04-22 15:45:01 +0300

    feat: implement func get times of day

--- photo-filter/main.js
+++ photo-filter/main.js
@@ -7,3 +7,20 @@ const initState = {
     hue: { value: 0, suffix: 'deg'},
   },
 };
+
+const getTimesOfDay = () => {
+  const hour = new Date().getHours();
+  
+  let timesOfDay;
+  if (hour >= 6 && hour <= 11) {
+    timesOfDay = 'morning';
+  } else if (hour >= 12 && hour <= 17) {
+    timesOfDay = 'day';
+  } else if (hour >= 18 && hour <= 23) {
+    timesOfDay = 'evening';
+  } else if (hour >= 0 && hour <= 5) {
+    timesOfDay = 'night';
+  }
+
+  return timesOfDay;
+};
