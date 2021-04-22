e0be0e0869b84e60a392c3c77297b955b6f486f0
Rasskris <rassoshenko.kristina@yandex.ru>
2021-04-22 15:41:15 +0300

    init: start photo-filter task

---
+++ photo-filter/main.js
@@ -0,0 +1,9 @@
+const initState = {
+  filter: {
+    blur: { value: 0, suffix: 'px' },
+    invert: { value: 0, suffix: '%' },
+    sepia: { value: 0, suffix: '%' },
+    saturate: { value: 100, suffix: '%'},
+    hue: { value: 0, suffix: 'deg'},
+  },
+};
