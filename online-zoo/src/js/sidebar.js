385560d3c4751b1e4cfd1065e18f500f64812e10
Rasskris <rassoshenko.kristina@yandex.ru>
2021-05-06 05:11:01 +0300

    feat: implement sidebar

--- online-zoo/src/js/sidebar.js
+++ online-zoo/src/js/sidebar.js
@@ -1,6 +1,23 @@
 const sidebarBtn = document.querySelector('.sidebar__btn');
 
-sidebarBtn.addEventListener('click', ({ target }) => {
-  const sidebarWrapper = target.previousElementSibling;
-  sidebarWrapper.classList.toggle('sidebar__wrapper_active');
-})
+const callListener = () => {
+  if (!sidebarBtn) {
+    return;
+  }
+  sidebarBtn.addEventListener('click', ({ target }) => {
+    const sidebarWrapper = target.nextElementSibling;
+    sidebarWrapper.classList.toggle('sidebar__wrapper_active');
+  });
+};
+callListener();
+// const sidebar = document.querySelector('.sidebar');
+
+// const domRect = sidebar.getBoundingClientRect();
+
+// window.onscroll = () => {
+//   if (window.pageYOffset > 200) {
+//     sidebar.classList.add('scroll_active');
+//   } else {
+//     sidebar.classList.remove('scroll_active');
+//   }
+// };
