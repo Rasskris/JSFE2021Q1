ae5113220570114c9ef4874eb587bcb603bbf585
Rasskris <rassoshenko.kristina@yandex.ru>
2021-05-06 05:14:37 +0300

    feat: add imports other js file

--- online-zoo/src/main.js
+++ online-zoo/src/main.js
@@ -1,6 +1,16 @@
 import './scss/main.scss';
 import './js/tooltip';
 import './js/spoiler';
+import './js/sidebar';
+import './js/sliderPets';
+import './js/sliderTest';
+import './js/carousel';
+import './js/modal';
+import './js/formFeedback';
+import './js/formDonat';
+import './js/formPay';
+import './js/map';
+import './js/sliderZoos';
 
 const hello = 'Hello Dolly';
 console.log('test:', hello);
