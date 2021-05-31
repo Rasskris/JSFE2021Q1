ff3550d1e9c7597019013e91d8c9e85c706e2e5a
Rasskris <rassoshenko.kristina@yandex.ru>
2021-05-31 20:54:45 +0300

    feat: add game-settings params

---
+++ match-match-game/src/params/game-settings.ts
@@ -0,0 +1,22 @@
+export const category = {
+  settingTitle: 'Cards Category',
+  settingDesc: 'Select game cards category',
+  settingType: 'category',
+  dataList: [
+    { settingData: 'fruits', nameItemList: 'Fruits', imgName: 'fruits' },
+    { settingData: 'animals', nameItemList: 'Animals', imgName: 'animals' },
+    { settingData: 'countries', nameItemList: 'Countries', imgName: 'countries' },
+    { settingData: 'flowers', nameItemList: 'Flowers', imgName: 'flowers' },
+  ],
+}; 
+
+export const difficulty = {
+  settingTitle: 'Difficulty',
+  settingDesc: 'Select game type',
+  settingType: 'difficulty',
+  dataList: [
+    { settingData: '9', nameItemList: 'Easy (18 cards)', imgName: 'easy' },
+    { settingData: '12', nameItemList: 'Medium (24 cards)', imgName: 'medium' },
+    { settingData: '15', nameItemList: 'Hard (30 cards)', imgName: 'hard' },
+  ],
+}
