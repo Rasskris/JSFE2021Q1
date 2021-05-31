527279c3a7b2b06d6d6ca1b68d4b01c9e6632865
Rasskris <rassoshenko.kristina@yandex.ru>
2021-05-31 20:48:21 +0300

    feat: add index file for common export handlers

---
+++ match-match-game/src/controllers/index.ts
@@ -0,0 +1,7 @@
+export { default as handleSettingClick } from  './handler-setting/handler-setting';
+export { default as handleBtnClick } from './handler-btn-click/handler-btn-click';
+export { default as handleSubmit } from './handler-submit/handler-submit';
+export { default as handleInput } from './handler-input/handler-input';
+export { default as handleUploadImg } from './handler-upload-img/handler-upload-img';
+export { default as handleScoreLink } from './handler-score-link/hadler-score-link';
+export { default as handleLogout } from './handler-logout/handler-logout'
