3f9f344f27e6890a57d936d1335b350788c29f30
Rasskris <rassoshenko.kristina@yandex.ru>
2021-06-16 00:24:25 +0300

    feat: add base component

---
+++ async-race/src/components/base-component/base-component.ts
@@ -0,0 +1,9 @@
+export default class BaseComponent {
+  readonly element: HTMLElement;
+
+  constructor(tag: keyof HTMLElementTagNameMap = 'div', className: string) {
+    this.element = document.createElement(tag);
+    this.element.classList.add(className);
+    this.element.setAttribute('id', className);
+  }
+}
