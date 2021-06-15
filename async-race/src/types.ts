60d79e19de2e07c9857c5daadba7e29ff0eeed7b
Rasskris <rassoshenko.kristina@yandex.ru>
2021-06-16 00:32:26 +0300

    feat: add types

---
+++ async-race/src/types.ts
@@ -0,0 +1,5 @@
+// import Loader from './libs/loader';
+
+// export type EventHandler = (target: HTMLElement, loader: Loader) => Promise<void> | void;
+export type DriveResponse = { success: boolean };
+export type ResponseStartEngine = { velocity: number, distance: number };
