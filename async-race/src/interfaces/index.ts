7679947ff2d79cc85ed94d3e02b30bbddca152a0
Rasskris <rassoshenko.kristina@yandex.ru>
2021-06-16 00:39:13 +0300

    feat: add index file for export interfaces

---
+++ async-race/src/interfaces/index.ts
@@ -0,0 +1,7 @@
+export { default as Car } from './car';
+export { default as RequestParameters } from './request-parameters';
+export { default as FormParameters } from './form-parameters';
+export { default as Winner } from './winner';
+export { default as Store } from './store';
+export { default as DrivePromise } from './drive-promise';
+export { default as MessageParams } from './message-params';
