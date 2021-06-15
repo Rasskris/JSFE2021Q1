11c90bc9a3ce0606177e8b36099e5641ae65cb2a
Rasskris <rassoshenko.kristina@yandex.ru>
2021-06-16 00:41:27 +0300

    feat: add constants

---
+++ async-race/src/constants.ts
@@ -0,0 +1,27 @@
+export const paramsOfCreateCarForm = {
+  classNameForm: 'form-create',
+  btnClassName: 'btn-create',
+  btnTxtContent: 'create',
+};
+
+export const paramsOfUpdateCarForm = {
+  classNameForm: 'form-update',
+  btnClassName: 'btn-update',
+  btnTxtContent: 'update',
+};
+
+export const carNames = ['Kia', 'Hyundai', 'Honda', 'Land Rover', 'Toyota', 'Mitsubishi', 'Lexus', 'Suzuki', 'Nissan', 'Infinity', 'Peugeot'];
+
+export const carModels = ['stinger', 'sonata', 'civic', 'discovery', 'camry', 'pajero', 'lx 570', 'swift', 'skyline', 'q30', '208'];
+
+export const garageURL = 'http://127.0.0.1:3000/garage';
+
+export const winnersURL = 'http://127.0.0.1:3000/winners';
+
+export const countCarsPerPage = 7;
+
+export const countWinnersPerPage = 10;
+
+export const garagePageName = 'garagePage';
+
+export const winnersPageName = 'winnersPage';
